b02c0102a1dd35b64faa75a051a8db733b1a7d28
Lucho <Lucianoceles@gmail.com>
2024-04-16 22:02:36 -0300

    arregle el style y cree la funcion para poder utilizar el Nav(No funciona)

--- App/app.mjs
+++ App/app.mjs
@@ -1,32 +1,32 @@
-const app = () =>{
-    
+const app = () => {
+
     const $div = document.getElementById("div-main");
     const $inicio = document.getElementById("id-inicio");
     const $cartelera = document.getElementById("id-cartelera");
     const $combos = document.getElementById("id-combos");
 
-    const CrearA = (id,href,text) =>{
+    const CrearA = (id, href, text) => {
         let $a = document.createElement("a");
-        $a.setAttribute("id",id);
+        $a.setAttribute("id", id);
         $a.href = href;
         $a.text = text;
         return $a;
     }
 
-    const CrearList = (a) =>{
+    const CrearList = (a) => {
         let $ul = document.createElement("ul");
         let $li = document.createElement("li");
         $li.appendChild(a);
         $ul.appendChild($li);
         return $ul;
     }
+
     $div.appendChild(CrearList(CrearA("id-inicio", "#inicio", "INICIO")));
     $div.appendChild(CrearList(CrearA("id-cartelera", "#cartelera", "CARTELERA")));
     $div.appendChild(CrearList(CrearA("id-combos", "#combos", "COMBOS")));
 
-
-    const NavElements =(id) =>{
-        switch(id){
+    const NavElements = (id) => {
+        switch (id) {
             case "id-inicio":
                 $inicio.style.display = "block";
                 $cartelera.style.display = "none";
@@ -44,6 +44,16 @@ const app = () =>{
                 break;
         }
     }
+
+    $inicio.addEventListener("click", () => {
+        NavElements("id-inicio");
+    });
+    $cartelera.addEventListener("click", () => {
+        NavElements("id-cartelera");
+    });
+    $combos.addEventListener("click", () => {
+        NavElements("id-combos");
+    });
 
 }
 
--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 const $DOM = document;
 import app from "./App/app.mjs";
 
-$DOM.addEventListener("DOMContentLoaded",()=>{
+$DOM.addEventListener("DOMContentLoaded", () => {
     app();
 });
