c4a9cc382e03949acc17536f53a4ba6990e7c0e4
Lucho <Lucianoceles@gmail.com>
2024-04-24 20:43:08 -0300

    me olvide de crear la seccion mediante js

--- App/app.mjs
+++ App/app.mjs
@@ -1,9 +1,9 @@
 import CargarInicioApp from "./componentes/cargaInicio.mjs";
 import navApp from "./componentes/nav.mjs";
+import carruselApp from "./componentes/seccionCarrusel.mjs";
 import inicioApp from "./componentes/seccionInicio.mjs";
 import combosApp from "./componentes/seccionCombos.mjs";
 import carteleraApp from "./componentes/seccionCartelera.mjs";
-import carruselApp from "./componentes/seccionCarrusel.mjs";
 
 const appFunc = () => {
     CargarInicioApp();
