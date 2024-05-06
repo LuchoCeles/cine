805c9debdeb3783c97719eb2879b4d8673e08712
Hilet Aulas <hilet@HILET.LOCAL>
2024-05-06 09:06:30 -0300

    fetch

--- services/Fetch.mjs
+++ services/Fetch.mjs
@@ -1,6 +1,6 @@
 const backendurl = "http://localhost:8080/";
 export async function Get (url, dataObject){
-    const objString ='?' + new URLSearchParams(dataObject).toString();
+    const objString = '?' + new URLSearchParams(dataObject).toString();
     return await fetch( backendurl + url + objString,{
         method:'GET',
         mode:'cors'
